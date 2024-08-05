6ef66813027c2c137a74302a1686f06dc69c2494
kinoppol <kinoppol@hotmail.com>
2024-08-05 17:11:40 +0700

    Update update.php

--- update.php
+++ update.php
@@ -5,7 +5,7 @@ define('UPDATER_PATH', str_replace('\\','/',dirname(__FILE__)).'/');
 print UPDATER_PATH;
 //exit();
 $url = "https://github.com/kinoppol/ret/archive/refs/heads/main.zip";
-$zip_file = UPDATER_PATH."/update/tp.zip";
+$zip_file = UPDATER_PATH."/update/temp.zip";
 
 $zip_resource = fopen($zip_file, "w");
 
